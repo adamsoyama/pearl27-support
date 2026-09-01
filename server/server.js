7db29b9bb98be2103174b5813aca860b8e943a7a
adamsoyama <oyama.adams@gmail.com>
2026-09-01 13:45:39 +0100

    Made few changes

--- server/server.js
+++ server/server.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 const multer = require("multer");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "../client/dist")));
 
 const upload = multer({
   storage: multer.memoryStorage(),
